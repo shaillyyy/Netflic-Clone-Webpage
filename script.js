7d5aac5fed6fd47584f38c21c45e2df654280bf5
Shailly Singh <121657750+shaillyyy@users.noreply.github.com>
2025-10-05 23:05:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const movies = [
+  {
+    title: 'Stranger Things',
+    img: 'assets/strangerthings.jpeg',
+    video: 'https://www.youtube.com/embed/b9EkMc79ZSU'
+  },
+  {
+    title: 'Money Heist',
+    img: 'assets/moneyheist.jpeg',
+    video: 'https://www.youtube.com/embed/_InqQJRqGW4'
+  },
+  {
+    title: 'The Crown',
+    img: 'assets/crown.jpeg',
+    video: 'https://www.youtube.com/embed/JWtnJjn6ng0'
+  },
+  {
+    title: 'Breaking Bad',
+    img: 'assets/breakingbad.jpeg',
+    video: 'https://www.youtube.com/embed/HhesaQXLuRY'
+  }
+];
